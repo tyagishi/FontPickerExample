6179563f59576f6661e6d0451704b2be57a08cb9
Tomoaki Yagishita <tyagishi@gmail.com>
2021-01-10 08:49:10 +0900

    Initial Commit

---
+++ FontPickerExample/FontPickerExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  FontPickerExampleApp.swift
+//
+//  Created by : Tomoaki Yagishita on 2021/01/10
+//  © 2021  SmallDeskSoftware
+//
+
+import SwiftUI
+
+@main
+struct FontPickerExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
